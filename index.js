cf9f35b77e8de6170e45b00e005b3fb2aaf3e88d
karolwasemann <kw.kwaq@gmail.com>
2021-05-10 09:09:33 +0200

    scroll down detection js

--- index.js
+++ index.js
@@ -3,4 +3,22 @@ import './style.css';
 const directions = {
   top: '🔼',
   bottom: '🔽'
-}
+};
+
+const direction = document.querySelector('.direction');
+const elevator = document.querySelector('.elevator');
+let lastScroll = elevator.scrollTop;
+
+elevator.addEventListener(
+  'scroll',
+  function() {
+    var st = elevator.scrollTop;
+    if (st > lastScroll) {
+      direction.innerHTML = `Kierunek: ${directions.bottom}`;
+    } else {
+      direction.innerHTML = `Kierunek: ${directions.top}`;
+    }
+    lastScroll = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+  },
+  false
+);
